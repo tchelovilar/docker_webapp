52967ef8df6fb7b2368f3b3b4ce747ef0efacb9b
vilar <marcelo.vilar@rivendel.com.br>
2018-03-16 16:21:33 -0300

    changed version

--- src/index.php
+++ src/index.php
@@ -41,6 +41,6 @@ echo "  \n";
 echo "<br>";
 echo 'Total Time: '.number_format($total_time,2).' seconds.';
 
-echo '<br><br><font size="1">Version 0.02</font>';
+echo '<br><br><font size="1">Version 0.03</font>';
 ?>
 
